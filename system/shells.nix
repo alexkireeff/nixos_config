b199d0efb4674e83e940889365a42e196d41a631
user <user@computer>
2022-08-02 21:48:04 -0400

    update

--- system/shells.nix
+++ system/shells.nix
@@ -21,5 +21,22 @@
 
       shellHook = "${pkgs.zsh}/bin/zsh; exit";
     };
+
+    bin = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        # debuggers
+        rr
+        gef
+        ghidra # eventually only need ghidra
+
+        # python exploit developement libraries
+        python3
+        python3Packages.pwntools
+        python3Packages.angr
+      ];
+
+      # https://www.reddit.com/r/suckless/comments/m0hke6/ghidra_is_not_displayed_in_dwm/
+      shellHook = "export _JAVA_AWT_WM_NONREPARENTING=1; ${pkgs.zsh}/bin/zsh; exit";
+    };
   };
 }
