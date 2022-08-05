418313dd8b88df88788205dff145fb2c51ee2e86
user <user@computer>
2022-08-05 01:19:42 -0400

    * added ability to patch (on top of the already existing ability to   compile)

--- system/shells.nix
+++ system/shells.nix
@@ -24,6 +24,12 @@
 
     bin = pkgs.mkShell {
       buildInputs = with pkgs; [
+        # bin for patching
+        patchelf
+        # libraries for patching
+        glibc
+        openssl
+
         # compiler
         gcc
 
@@ -41,16 +47,40 @@
       # https://www.reddit.com/r/suckless/comments/m0hke6/ghidra_is_not_displayed_in_dwm/
       shellHook = ''
         export _JAVA_AWT_WM_NONREPARENTING=1
+        export GLIBC=${pkgs.glibc}/lib
+        export CRYPTO=${pkgs.openssl.out}/lib
+
+        echo "Patching with patchelf";
+
+        echo "get interpreter";
+        echo "patchelf --print-interpreter BIN";
+        echo "set interpreter";
+        echo "patchelf --set-interpreter INTERPRETER BIN";
+
+        echo "get rpath";
+        echo "patchelf --print-rpath BIN";
+        echo "set rpath";
+        echo "patchelf --set-rpath RPATH BIN";
+
+        echo "";
+
+        echo "Compiling with gcc";
+
         echo "disable ASLR:";
         echo "sudo bash -c 'echo 0 > /proc/sys/kernel/randomize_va_space'";
+
         echo "disable buffer overflow protection";
         echo "-D_FORTIFY_SOURCE=0";
+
         echo "disable executable stack:";
         echo "-z execstack";
+
         echo "disable stack canaries:";
         echo "-fno-stack-protector";
+
         echo "disable Position Independent Executables:";
         echo "-no-pie";
+
         echo "compile 32 bit:";
         echo "-m32";
         ${pkgs.zsh}/bin/zsh
