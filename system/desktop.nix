f7fc0ed673a54dda7c24e9dbd499acf25d261e52
Alex Kireeff <alexkireeff@gmail.com>
2022-08-16 22:52:23 -0400

    * added lspci

--- system/desktop.nix
+++ system/desktop.nix
@@ -13,6 +13,8 @@ in {
   imports = ["${CD}/base.nix"];
   # TODO want to be able to recover from bad config, do that by making a service run during initrd that pulls from git repo and runs update
 
+  environment.systemPackages = with pkgs; [pciutils];
+
   # nvidia driver
   services.xserver.videoDrivers = ["nvidia"];
 
