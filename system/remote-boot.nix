ea51c36bf925297ae59d83f2d340b6d6784f6514
Alex Kireeff <alexkireeff@gmail.com>
2022-08-17 21:58:57 -0400

    * lets copy the *exact* code they have on the wiki

--- system/remote-boot.nix
+++ system/remote-boot.nix
@@ -4,59 +4,36 @@
   lib,
   ...
 }: {
-  # TODO want to be able to recover from bad config
-  # do that with "macros" that are in initrd
-  # macro 1: rollback
-  # macro 2: input password
+# copy your onion folder
+boot.initrd.secrets = {
+  "/etc/tor/onion/bootup" = /home/user/tor/onion; # maybe find a better spot to store this.
+};
 
-  # initrd ssh setup
-  boot.initrd.network.enable = true;
-  boot.initrd.network.ssh = {
-    enable = true;
-    port = 22;
-    authorizedKeys = config.users.users.user.openssh.authorizedKeys.keys;
-    # TODO PasswordAuthentication no; Protocol 2; X11Forwarding no; PubkeyAuthentication yes
-    hostKeys =
-      if (builtins.pathExists /home/user/.ssh/initrd_ssh_host_key)
-      then ["/home/user/.ssh/initrd_ssh_host_key"]
-      else throw "no initrd ssh file";
-  };
+# copy tor to you initrd
+boot.initrd.extraUtilsCommands = ''
+  copy_bin_and_libs ${pkgs.tor}/bin/tor
+'';
 
-  # initrd copy secrets
-  boot.initrd.secrets = {
-    "/etc/tor/onion/bootup" =
-      if (builtins.pathExists /home/user/tor/onion)
-      then /home/user/tor/onion
-      else throw "no initrd onion file";
-  };
+# start tor during boot process
+boot.initrd.network.postCommands = let
+  torRc = (pkgs.writeText "tor.rc" ''
+    DataDirectory /etc/tor
+    SOCKSPort 127.0.0.1:9050 IsolateDestAddr
+    SOCKSPort 127.0.0.1:9063
+    HiddenServiceDir /etc/tor/onion/bootup
+    HiddenServicePort 22 127.0.0.1:22
+  '');
+in ''
+  echo "tor: preparing onion folder"
+  # have to do this otherwise tor does not want to start
+  chmod -R 700 /etc/tor
 
-  # initrd copy tor, haveged
-  boot.initrd.extraUtilsCommands = ''
-    copy_bin_and_libs ${pkgs.tor}/bin/tor
-    copy_bin_and_libs ${pkgs.haveged}/bin/haveged
-  '';
+  echo "make sure localhost is up"
+  ip a a 127.0.0.1/8 dev lo
+  ip link set lo up
 
-  # run tor during boot process
-  # TODO does this keep on running tor? idk
-  boot.initrd.network.postCommands = let
-    torrc = pkgs.writeText "tor.rc" ''
-      DataDirectory /etc/tor
-      SOCKSPort 127.0.0.1:9050 IsolateDestAddr
-      SOCKSPort 127.0.0.1:9063
-      HiddenServiceDir /etc/tor/onion/bootup
-      HiddenServicePort 22 127.0.0.1:22
-    '';
-  in ''
-    # tor needs 700 on folder
-    chmod -R 700 /etc/tor
-
-    # TODO necessary?
-    #ip a a 127.0.0.1/8 dev lo
-    #ip link set lo up
-
-    haveged -F &
-
-    tor -f ${torrc} --verify-config
-    tor -f ${torrc} &
-  '';
+  echo "tor: starting tor"
+  tor -f ${torRc} --verify-config
+  tor -f ${torRc} &
+'';
 }
