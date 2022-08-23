43e7813d9208ecd432f715a71971fd5597faaf82
Alex Kireeff <alexkireeff@gmail.com>
2022-08-23 16:43:42 -0400

    * updated remote boot to take duckdns script in nix dir

--- system/remote-boot.nix
+++ system/remote-boot.nix
@@ -4,30 +4,44 @@
   lib,
   ...
 }: {
-# ssh setup
-boot.initrd.network.enable = true;
-boot.initrd.network.ssh = {
-  enable = true;
-  port = 22;
-  authorizedKeys = config.users.users.user.openssh.authorizedKeys.keys;
-  hostKeys = [ "/home/user/.ssh/initrd_ssh_host_key" ];
-};
+  # ssh setup
+  boot.initrd.network.enable = true;
+  boot.initrd.network.ssh = {
+    enable = true;
+    port = 22;
+    authorizedKeys = config.users.users.user.openssh.authorizedKeys.keys;
+    hostKeys = ["/home/user/.ssh/initrd_ssh_host_key"];
+  };
 
-# copy your onion folder
-boot.initrd.secrets = {
-  "/etc/duckdnsscript.sh" = /home/user/duckdnsscript.sh;
-  "/etc/ssl/certs/ca-certificates.crt" = null;
-};
+  # copy your onion folder
+  boot.initrd.secrets = {
+    "/etc/nixos/duckdnsscript.sh" = null;
+    "/etc/ssl/certs/ca-certificates.crt" = null;
+  };
 
-# copy tor to you initrd
-boot.initrd.extraUtilsCommands = ''
-  copy_bin_and_libs ${pkgs.bash}/bin/bash
-  copy_bin_and_libs ${pkgs.pkgsStatic.curl}/bin/curl
-'';
+  # copy tor to you initrd
+  boot.initrd.extraUtilsCommands = ''
+    copy_bin_and_libs ${pkgs.bash}/bin/bash
+    copy_bin_and_libs ${pkgs.pkgsStatic.curl}/bin/curl
+  '';
 
-# start tor during boot process
-boot.initrd.network.postCommands = ''
-  echo "updating duckdns ip"
-  bash /etc/duckdnsscript.sh
-'';
+  # start tor during boot process
+  boot.initrd.network.postCommands = ''
+    echo "updating duckdns ip"
+    bash /etc/nixos/duckdnsscript.sh
+  '';
+
+  systemd.services = {
+    dynamic-dns-updater = {
+      path = [
+        pkgs.bash
+        pkgs.pkgsStatic.curl
+      ];
+      script = "bash /etc/nixos/duckdnsscript.sh";
+      serviceConfig = {
+        User = config.users.users.default.name;
+      };
+      startAt = "hourly";
+    };
+  };
 }
