794f2ace300872e24e8de3ee12a4b1c3b3cd05a9
Alex Kireeff <alexkireeff@gmail.com>
2022-09-29 15:55:57 -0400

    * commented out nixFlakes to see if that was the change nixos keeps on   throwing an error about * https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/aliases.nix#L784

--- system/base.nix
+++ system/base.nix
@@ -182,7 +182,7 @@ in {
     };
 
     # TODO FUTURE remove flake experimental when not experimental anymore
-    package = pkgs.nixFlakes;
+    # package = pkgs.nixFlakes;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
