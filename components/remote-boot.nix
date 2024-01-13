ad602d5f3559b14296fde582dac8ecfb8acb031b
Alex Kireeff <alexkireeff@gmail.com>
2024-01-13 00:11:16 -0500

    * remote-boot uses impure-info

--- components/remote-boot.nix
+++ components/remote-boot.nix
@@ -4,9 +4,7 @@
   lib,
   impure-info,
   ...
-}: let
-  duckdns_url_file_path = "/etc/nixos/duckdns_url";
-in {
+}: {
   boot.initrd = {
     # copy programs to initrd
     extraUtilsCommands = ''
@@ -17,52 +15,27 @@ in {
     network = {
       enable = true;
 
-      # run during boot process
-      # https://www.duckdns.org/install.jsp
+      # after initrd done, run this command
       postCommands = ''
         curl --cacert /etc/ssl/certs/ca-certificates.crt "$(cat /etc/nixos/duckdns_url)" > /dev/null
       '';
 
       # ssh setup
-      ssh = let
-        initrd_ssh_host_key_file_path = "/etc/nixos/initrd_ssh_host_key";
-      in {
+      ssh = {
         enable = true;
         authorizedKeys = config.users.users.user.openssh.authorizedKeys.keys;
-        hostKeys =
-          if (builtins.pathExists initrd_ssh_host_key_file_path)
-          then [(builtins.toPath initrd_ssh_host_key_file_path)]
-          else
-            throw ''
-              missing initrd ssh host key file
-              Do:
-                sudo ssh-keygen -t ed25519 -a 100 -N "" -C "initrd_ssh_key" -f ${initrd_ssh_host_key_file_path}
-                sudo chown user:users ${initrd_ssh_host_key_file_path} ${initrd_ssh_host_key_file_path}.pub
-                chmod 600 ${initrd_ssh_host_key_file_path}
-                chmod 644 ${initrd_ssh_host_key_file_path}.pub
-            '';
+        hostKeys = impure-info.initrd_ssh_host_key_file_path;
       };
     };
 
     # copy files to initrd
     secrets = {
-      "/etc/nixos/duckdns_url" =
-        if (builtins.pathExists duckdns_url_file_path)
-        then duckdns_url_file_path
-        else
-          throw ''
-            missing duckdns url file
-            Do:
-              https://www.duckdns.org/install.jsp?tab=hardware
-              get the domain and token
-              echo "https://www.duckdns.org/update?domains=$DOMAIN&token=$TOKEN&ip=" | sudo tee ${duckdns_url_file_path}
-              sudo chmod 400 ${duckdns_url_file_path}
-          '';
-
+      "/etc/nixos/duckdns_url" = impure-info.duckdns_url_file_path;
       "/etc/ssl/certs/ca-certificates.crt" = builtins.toPath "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
     };
   };
 
+  # update ddns every minute once decrypted
   systemd.services = {
     ddns-updater = {
       path = with pkgs; [
