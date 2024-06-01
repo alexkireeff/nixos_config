564211ec89594157f780bedb6b107a31c1c215d1
Alex Kireeff <alexkireeff@gmail.com>
2024-06-01 00:49:09 -0400

    * use my repo now to test the hardened stuff

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:alexkireeff/nixpkgs/hardened";
 
     home-manager = {
       url = "github:nix-community/home-manager";
