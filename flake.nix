7575b0892b4510df4323543284bdb184d8675382
Alex Kireeff <alexkireeff@gmail.com>
2024-06-04 15:45:56 -0400

    * need to update config

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     pkgs = import nixpkgs {
       system = "x86_64-linux";
       config.allowUnfree = true;
-      config.contentAddressedByDefault = true;
+      # config.contentAddressedByDefault = true;
     };
     impure-info = {
       /*
