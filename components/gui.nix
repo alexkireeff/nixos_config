dc508986719b6d4fe973acbe501db0fb1bd8acb5
Alex Kireeff <alexkireeff@gmail.com>
2024-06-08 19:45:56 -0400

    * yambar

--- components/gui.nix
+++ components/gui.nix
@@ -236,71 +236,6 @@ in {
         };
       };
 
-      programs.i3status-rust = {
-        bars.bottom = {
-          blocks = [
-            {
-              block = "battery";
-              empty_format = " BAT $percentage {$time|} ";
-              format = " BAT $percentage {$time|} ";
-              full_format = " BAT $percentage {$time|} ";
-              interval = 60;
-              not_charging_format = " BAT $percentage {$time|} ";
-            }
-            {
-              block = "cpu";
-              format = " CPU $utilization ";
-              interval = 1;
-            }
-            {
-              block = "disk_space";
-              format = " DISK $used/$total ";
-              path = "/";
-            }
-            {
-              block = "memory";
-              format = " MEM $mem_used/$mem_total ";
-              format_alt = " SWP $swap_used/$swap_total ";
-              interval = 5;
-            }
-            {
-              block = "net";
-              format = " {AP $ssid|LAN} {$ip|$ipv6}{ $signal_strength|} ";
-              click = [
-                {
-                  button = "left";
-                  cmd = "${pkgs.alacritty}/bin/alacritty -e nmtui";
-                }
-              ];
-            }
-            {
-              block = "sound";
-              max_vol = 100;
-              show_volume_when_muted = true;
-              step_width = 1;
-            }
-            {
-              block = "backlight";
-              cycle = [1 100];
-              device = "intel_backlight";
-              maximum = 100;
-              minimum = 1;
-              step_width = 1;
-            }
-            {
-              block = "time";
-              format = " $timestamp.datetime(f:'%Y/%m/%d %H:%M:%S') ";
-              interval = 1;
-              timezone = "America/New_York";
-            }
-          ];
-
-          settings.theme.theme = "plain";
-        };
-
-        enable = true;
-      };
-
       programs.swaylock.settings = {
         show-failed-attempts = false;
 
@@ -340,12 +275,97 @@ in {
         disable-caps-lock-text = true;
       };
 
+      # programs.i3status-rust = {
+      #   bars.bottom = {
+      #     blocks = [
+      #       {
+      #         block = "battery";
+      #         empty_format = " BAT $percentage {$time|} ";
+      #         format = " BAT $percentage {$time|} ";
+      #         full_format = " BAT $percentage {$time|} ";
+      #         interval = 60;
+      #         not_charging_format = " BAT $percentage {$time|} ";
+      #       }
+      #       {
+      #         block = "cpu";
+      #         format = " CPU $utilization ";
+      #         interval = 1;
+      #       }
+      #       {
+      #         block = "disk_space";
+      #         format = " DISK $used/$total ";
+      #         path = "/";
+      #       }
+      #       {
+      #         block = "memory";
+      #         format = " MEM $mem_used/$mem_total ";
+      #         format_alt = " SWP $swap_used/$swap_total ";
+      #         interval = 5;
+      #       }
+      #       {
+      #         block = "net";
+      #         format = " {AP $ssid|LAN} {$ip|$ipv6}{ $signal_strength|} ";
+      #         click = [
+      #           {
+      #             button = "left";
+      #             cmd = "${pkgs.alacritty}/bin/alacritty -e nmtui";
+      #           }
+      #         ];
+      #       }
+      #       {
+      #         block = "sound";
+      #         max_vol = 100;
+      #         show_volume_when_muted = true;
+      #         step_width = 1;
+      #       }
+      #       {
+      #         block = "backlight";
+      #         cycle = [1 100];
+      #         device = "intel_backlight";
+      #         maximum = 100;
+      #         minimum = 1;
+      #         step_width = 1;
+      #       }
+      #       {
+      #         block = "time";
+      #         format = " $timestamp.datetime(f:'%Y/%m/%d %H:%M:%S') ";
+      #         interval = 1;
+      #         timezone = "America/New_York";
+      #       }
+      #     ];
+
+      #     settings.theme.theme = "plain";
+      #   };
+
+      #   enable = true;
+      # };
+
+      programs.yambar = {
+        enable = true;
+        settings = {
+          bar = {
+            location = "top";
+            height = 26;
+            background = "00000066";
+
+            right = [
+              {
+                clock.content = [
+                  {
+                    string.text = "{time}";
+                  }
+                ];
+              }
+            ];
+          };
+        };
+      };
+
       wayland.windowManager.river = {
         enable = true;
         systemd.enable = true;
         xwayland.enable = false;
         extraConfig = builtins.readFile ./configs/river.config;
-        # TODO run i3status-rust `${pkgs.i3status-rust}/bin/i3status-rs config-bottom.toml`
       };
     };
   };
